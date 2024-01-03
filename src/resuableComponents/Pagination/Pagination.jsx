e3542d06dcb6fc58a3a1c9a620b0af20d90db047
NITIN SHARMA <nitinsharma@NITINs-MacBook-Pro.local>
2024-01-03 22:26:38 +0530

    fixing disable

--- src/resuableComponents/Pagination/Pagination.jsx
+++ src/resuableComponents/Pagination/Pagination.jsx
@@ -6,7 +6,7 @@ const Pagination = ({
         <div className="w-full flex-none flex items-center flex text-sm space-x-3 my-4">
             <button className="border-2 p-2 relative flex-none flex items-center justify-center cursor-pointer"
                 disabled={pageNumber === 1} onClick={(e => setPageNumber(e, pageNumber === 1 ? 1 : pageNumber - 1))}
-                style={pageNumber === 1 ? {backgroundColor: 'gray', cursor: 'none'}: null}>
+                style={pageNumber === 1 ? {backgroundColor: 'gray', cursor: 'unset'}: null}>
                     Previous
             </button>
             {
@@ -19,8 +19,8 @@ const Pagination = ({
                 })
             }
             <button className="border-2 p-2 relative flex-none flex items-center justify-center cursor-pointer"
-                onClick={(e => setPageNumber(e, pageNumber + 1))}
-                style={pageNumber === range.length ? {backgroundColor: 'gray', cursor: 'none'}: null}>
+                onClick={(e => setPageNumber(e, pageNumber + 1))} disabled={pageNumber === range.length}
+                style={pageNumber === range.length ? {backgroundColor: 'gray', cursor: 'unset'}: null}>
                     Next
             </button>
         </div>
